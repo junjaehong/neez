55f23a37bbc15c844441d6548e39311eb9b78558
상진 <sangji90@gmail.com>
2025-11-06 13:20:55 +0900

    pom 바꾸고 다시 시작하는중

--- BackEnd/neez/src/main/java/com/bbey/neez/entity/users.java
+++ BackEnd/neez/src/main/java/com/bbey/neez/entity/users.java
@@ -1,25 +1,26 @@
 package com.bbey.neez.entity;
 
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "users")
 public class Users {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idx;
-    private String id;
+    private Long idx; // PK
+
+    private String user_id;
     private String password;
     private String name;
-    private String email;
-    private String phone_number;
+
     private LocalDateTime created_at;
     private LocalDateTime updated_at;
+
+    // 방어용 세터 (Lombok 미작동 환경 대비)
+    public void setCreated_at(LocalDateTime created_at) { this.created_at = created_at; }
+    public void setUpdated_at(LocalDateTime updated_at) { this.updated_at = updated_at; }
 }
