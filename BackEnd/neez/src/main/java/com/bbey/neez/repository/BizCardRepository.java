4c475054a68d445ab7cf63bfa3c2d7286efb64c5
jaehong2353 <jaehong2353@gmail.com>
2025-11-09 16:18:19 +0900

    추후 통합시 문제 발생 예방을 위한 정리

--- BackEnd/neez/src/main/java/com/bbey/neez/repository/BizCardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bbey.neez.repository;
-
-import com.bbey.neez.entity.BizCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BizCardRepository extends JpaRepository<BizCard, Long> {
-}
--- BackEnd/neez/src/main/java/com/bbey/neez/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bbey.neez.repository;
-
-import com.bbey.neez.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
