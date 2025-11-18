1fa02af9e2672770c73e4b4995955bf0db0143d2
aremee <r.you.meee@gmail.com>
2025-11-18 16:47:47 +0900

    App.jsx 수정

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,36 +1,50 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
 import Start from './pages/Start';
 import Login from './pages/Login';
 import Join from './pages/Join';
 import Main from './pages/Main';
-import MyPage from './pages/MyPage';
-import MyCard from './pages/MyCard';
-import CardList from './pages/CardList';
+import Mypage from './pages/MyPage';
+import Mycard from './pages/Mycard';
+import Cardlist from './pages/Cardlist';
+import CameraCapture from './pages/CameraCapture';
+import CardInput from './pages/CardInput';
 import CardDetail from './pages/CardDetail';
-import HashtagList from './pages/HashtagList';
+import SttCardSelect from './pages/SttCardSelect';
+import SttIng from './pages/SttIng';
+import Setting from './pages/Setting';
+import Password from './pages/Password';
+import Email from './pages/Email';
 import './App.css';
-import { AppProvider } from './context/AppContext';
 
 function App() {
   return (
     <AppProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Start />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/mycard" element={<MyCard />} />
-          <Route path="/cardlist" element={<CardList />} />
-          <Route path="/hashtaglist" element={<HashtagList />} />
-          <Route path="/carddetail/:id" element={<CardDetail />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Start />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/mycard" element={<Mycard />} />
+            <Route path="/cardlist" element={<Cardlist />} />
+            <Route path="/carddetail/:id" element={<CardDetail />} />
+            <Route path="/cameracapture" element={<CameraCapture />} />
+            <Route path="/cardinput" element={<CardInput />} />
+            <Route path="/sttcardselect" element={<SttCardSelect />} />
+            <Route path="/stting" element={<SttIng />} />
+            <Route path="/setting" element={<Setting />} />
+            <Route path="/password" element={<Password />} />
+            <Route path="/email" element={<Email />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </Router>
     </AppProvider>
   );
 }
 
-export default App
+export default App;
