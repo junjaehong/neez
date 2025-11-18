ee63fc3c84404804f99445364109cddf51a3fa59
jaehong2353 <jaehong2353@gmail.com>
2025-11-18 10:56:09 +0900

    CORS문제해결

--- BackEnd/neez/src/main/java/com/bbey/neez/config/WebConfig.java
+++ BackEnd/neez/src/main/java/com/bbey/neez/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // 모든 엔드포인트
                         .allowedOrigins("http://localhost:5173")  // 프론트 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
